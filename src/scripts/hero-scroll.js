0b14a2eb6781e0f8e9cd8489fc3f0d9f116ea679
Max Reis <maxreis507@gmail.com>
2025-01-16 22:13:43 +0100

    feat: add product quickiview modal

---
+++ src/scripts/hero-scroll.js
@@ -0,0 +1,5 @@
+const ctaBtn = document.querySelector("#cta");
+const categoriesSection = document.querySelector("#categories");
+
+
+categoriesSection.scrollIntoView({ behavior: 'smooth' })
---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+
+
+const modal1 = document.querySelector('.open-modal');
+
+
+
+modal1.onclick = () => {
+    const modalId = modal1.getAttribute("data-modal");
+    document.getElementById(modalId).classList.remove("hidden")
+}
+
+
+const cm = document.querySelector('.close-modal')
+cm.onclick = () => {
+    const modalId = cm.getAttribute("data-modal");
+
+    document.getElementById(modalId).classList.add("hidden", "ease-out", "duration-300")
+}
