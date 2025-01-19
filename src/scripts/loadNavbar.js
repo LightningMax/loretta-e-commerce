846f6fb9c8bbe0da6f8fae72498e56520bdbfcae
Max Reis <maxreis507@gmail.com>
2025-01-19 15:39:47 +0100

    feat: add navbar component

---
+++ src/scripts/loadNavbar.js
@@ -0,0 +1,20 @@
+const loadNavbar = () => {
+    fetch("../src/components/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("#navbar").innerHTML = data;
+
+        const navbar = document.querySelector("#navbar")
+        const page = window.location.pathname
+
+        if (page.includes("index.html")) {
+            const hrefs = navbar.querySelectorAll("a");
+            hrefs.forEach((href) => {
+                console.log(href)
+                href.classList.replace("text-gray-900", "text-gray-50");
+            })
+        };
+    })
+}
+
+export default loadNavbar
